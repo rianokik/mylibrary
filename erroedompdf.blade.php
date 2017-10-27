eba5c7139ac01ff6b6bff2b07746dc099b738019
rianokik <rianokik2@gmail.com>
2017-10-27 15:32:58 +0700

    Create erroedompdf.blade.php

---
+++ erroedompdf.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title></title>
+	<style>
+			html
+			{
+				margin: 0px;
+				margin-bottom: 0px;
+			}
+			.di
+			{
+				display: inline-block;
+			}
+	</style>
+</head>
+<body>
+		<h3>REPORT ALL MAN POWER</h3>
+		<div class="di">
+			<table width="50%" style="font-size: 9; border-collapse: collapse; border: 0px;">
+				<tr>
+					<td style="border: 1px solid" rowspan="5" width="30%">
+						<center><div style="width: 3cm; height: 4cm; background-color: red"></div></center>
+					</td>
+					<th style="border: 1px solid" width="20%"><center>Nama</center></th>
+					<td style="border: 1px solid">: Okik Rian</td>
+				</tr>
+				<tr>
+					<th style="border: 1px solid"><center>Divisi</center></th>
+					<td style="border: 1px solid">: MIS</td>
+				</tr>
+				<tr>
+					<th style="border: 1px solid"><center>Grade</center></th>
+					<td style="border: 1px solid">: X-3</td>
+				</tr>
+				<tr>
+					<th style="border: 1px solid"><center>Masakerja</center></th>
+					<td style="border: 1px solid">: 3 tahun 9 bulan</td>
+				</tr>
+				<tr>
+					<th style="border: 1px solid"><center>Umur</center></th>
+					<td style="border: 1px solid">: 6 tahun 7 bulan</td>
+				</tr>
+			</table>
+		</div>
+		<div class="di">
+			<table width="50%" style="font-size: 9; border-collapse: collapse; border: 0px;">
+				<tr>
+					<td style="border: 1px solid" rowspan="5" width="30%">
+						<center><div style="width: 3cm; height: 4cm; background-color: red">&nbsp;</div></center>
+					</td>
+					<th style="border: 1px solid" width="20%"><center>Nama</center></th>
+					<td style="border: 1px solid">: Okik Rian</td>
+				</tr>
+				<tr>
+					<th style="border: 1px solid"><center>Divisi</center></th>
+					<td style="border: 1px solid">: MIS</td>
+				</tr>
+				<tr>
+					<th style="border: 1px solid"><center>Grade</center></th>
+					<td style="border: 1px solid">: X-3</td>
+				</tr>
+				<tr>
+					<th style="border: 1px solid"><center>Masakerja</center></th>
+					<td style="border: 1px solid">: 3 tahun 9 bulan</td>
+				</tr>
+				<tr>
+					<th style="border: 1px solid"><center>Umur</center></th>
+					<td style="border: 1px solid">: 6 tahun 7 bulan</td>
+				</tr>
+			</table>
+		</div>
+
+</body>
+</html>
